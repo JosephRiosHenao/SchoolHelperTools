5fa3ae0cd2e786e74e9ea708b037b8d4f7f32059
Joseph Rios Henao <joseph232005@gmail.com>
2021-10-21 06:54:24 -0500

    add procces print helper

--- Moles.py
+++ Moles.py
@@ -1,4 +1,7 @@
 gSoluto = float(input("Digite los gramos de soluto: "))
 PM = float(input("Digite el peso molecular: "))
 N = (gSoluto/PM)
-print("Los moles son ",N)
+print("N = gramos de soluto / peso molecular")
+print("N = ",gSoluto," / ",PM)
+print("N = ",N," N")
+print("Los moles son ",N," N")
